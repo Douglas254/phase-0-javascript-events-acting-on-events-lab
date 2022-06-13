bacd0f8a039c985805e97b32467e0e2e374b5c36
Douglas Obara <douglasobara97@gmail.com>
2022-06-13 16:19:03 +0300

    commit tree f8c2e5841a8b60c00ea32f5ac42df9efcd5cfee3

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
 // Your code here
+const dodger = document.getElementById("dodger");
+// Change dodger background color
+dodger.style.backgroundColor = "#FF69B4";
+
+// moving the element up
+// dodger.style.bottom = "100px";
+
+//  moves the DODGER to the left
+function moveDodgerLeft() {
+  const leftNumbers = dodger.style.left.replace("px", "");
+  const left = parseInt(leftNumbers, 10);
+
+  if (left > 0) {
+    dodger.style.left = `${left - 1}px`;
+  }
+}
+
+document.addEventListener("keydown", function (e) {
+  if (e.key === "ArrowLeft") {
+    moveDodgerLeft();
+  }
+});
+
+// moves the DODGER to the right
+function moveDodgerRight() {
+  var leftNumbers = dodger.style.left.replace("px", "");
+  var left = parseInt(leftNumbers, 10);
+  if (left < 360) {
+    dodger.style.left = `${left + 1}px`;
+  }
+}
+
+document.addEventListener("keydown", function (e) {
+  if (e.key === "ArrowRight") {
+    moveDodgerRight();
+  }
+});
